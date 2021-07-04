f3d3a7bd1da4803a92faaa12f94b7de05d3dfdfb
carlaespindola <carlaespindola00@gmail.com>
2021-07-04 12:24:48 -0300

    update teste map

--- src/main/kotlin/one/digitalinnovation/collections/TesteMap.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteMap.kt
@@ -1,2 +1,16 @@
 package one.digitalinnovation.collections
 
+fun main() {
+
+    val pair: Pair<String, Double> = Pair("João", 1000.0) //par
+    val map1 = mapOf(pair) //a chave e o valor gera um mapa
+
+    map1.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
+
+    println("-----------------------------")
+    val map2 = mapOf(
+        "Pedro" to 2500.0,
+        "Maria" to 3000.0
+    ) //recurso infix
+    map2.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
+}
