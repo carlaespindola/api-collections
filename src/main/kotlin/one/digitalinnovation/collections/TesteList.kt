f6035ec4fa911cab80fe74de1ba1de82d22ca481
carlaespindola <carlaespindola00@gmail.com>
2021-07-04 11:47:00 -0300

    updated test list, extract employee to separated file Funcionarios and created class Funcionarios

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -26,14 +26,3 @@ fun main() {
 
 }
 
-data class Funcionario(
-    val nome:String,
-    val salario:Double,
-    val tipoContratacao:String
-) {
-    override fun toString(): String =
-        """
-            Nome:    $nome
-            Salário: $salario
-        """.trimIndent()
-}
