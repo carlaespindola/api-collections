58d825b481a668b321009d85dc08020500dd5c71
carlaespindola <carlaespindola00@gmail.com>
2021-07-04 11:31:55 -0300

    update teste list using sortedBy

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,8 +1,8 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val joao = Funcionario("João", 1000.0)
-    val pedro = Funcionario("Pedro", 2000.0)
+    val joao = Funcionario("João", 2000.0)
+    val pedro = Funcionario("Pedro", 1500.0)
     val maria = Funcionario("Maria", 4000.0)
 
     val funcionarios = listOf(joao, pedro, maria)
@@ -11,6 +11,11 @@ fun main() {
 
     println("-----------------------")
     println(funcionarios.find {it.nome == "Maria" })
+
+    println("-----------------------")
+    funcionarios
+        .sortedBy { it.salario } //ordem menor ao maior
+        .forEach { println(it) } //operação coletora
 }
 
 data class Funcionario(
