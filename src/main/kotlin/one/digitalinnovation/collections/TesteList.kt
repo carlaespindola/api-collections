53e3bfd9980aa8bec558b335963e8e278da06974
carlaespindola <carlaespindola00@gmail.com>
2021-07-04 11:39:38 -0300

    update teste list using groupBy

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val joao = Funcionario("João", 2000.0)
-    val pedro = Funcionario("Pedro", 1500.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -16,11 +16,20 @@ fun main() {
     funcionarios
         .sortedBy { it.salario } //ordem menor ao maior
         .forEach { println(it) } //operação coletora
+
+    println("-----------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao } //tipo de agrupamento
+        .forEach { println(it) } //operação coletora, imprime o resultado obtido - é criado um MAPA, com chave e elementos
+
+    /*nesse exemplo, não conseguimos inserir mais um funcionario pois estamos usando a lista do api collections. Iremos ver essa inserção nas mutableList*/
+
 }
 
 data class Funcionario(
     val nome:String,
-    val salario:Double
+    val salario:Double,
+    val tipoContratacao:String
 ) {
     override fun toString(): String =
         """
