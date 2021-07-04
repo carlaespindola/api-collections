9a70ecb1deba9cecf70c348092860c9967dbe1db
carlaespindola <carlaespindola00@gmail.com>
2021-07-04 12:42:35 -0300

    created teste colecoes multaveis

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteColecoesMultaveis.kt
@@ -0,0 +1,33 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+
+    println("-------------LIST-----------------")
+    val funcionarios = mutableListOf(joao, maria) //lista multável, podemos alterar após a sua inicialização
+    funcionarios.forEach { println(it) }
+
+    println("-----------------------------")
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+
+    println("-----------------------------")
+    funcionarios.remove(joao)
+    funcionarios.forEach { println(it) }
+
+    println("-------------SET-----------------")
+    val funcionarioSet = mutableSetOf(joao)
+    funcionarioSet.forEach{ println(it)}
+
+    println("-----------------------------")
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach{ println(it)}
+
+    println("-----------------------------")
+    funcionarioSet.remove(maria)
+    funcionarioSet.forEach{ println(it)}
+}
